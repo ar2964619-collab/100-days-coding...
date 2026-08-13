595e8a8aca8c91591f8fae436589cb78ce1df9cb
Arya Vardhan <ar2964619@gmail.com>
2026-08-13 23:31:15 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,19 @@
+//Write a program to input two numbers and display their sum
+#include <stdio.h>
+
+int main(){
+int a, b ;
+scanf ("%d %d", &a , &b);
+printf  (" sum =%d", a+b);
+    return 0;
+}
+/*PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q1.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe     
+3
+4
+ sum =7
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q1.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe 
+-1
+20
+ sum =19*/
---
+++ Q2.c
@@ -0,0 +1,28 @@
+//Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include <stdio.h>
+
+int main(){
+int a, b;
+scanf ("%d %d",&a, &b);
+printf ("sum= %d\n" ,a+b);
+printf ("diffrence= %d\n" ,a -b);
+printf ("product= %d\n" ,a*b);
+printf ("quotient= %d\n" ,a/b);
+    return 0;
+}
+/*PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q2.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe 
+10
+2
+sum= 12
+diffrence= 8
+product= 20
+quotient= 5
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q2.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe 
+7
+3
+sum= 10
+diffrence= 4
+product= 21
+quotient= 2*/
