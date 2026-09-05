a18c70130c293c4972797160cbef23ea4793f61a
Arya Vardhan <ar2964619@gmail.com>
2026-09-06 00:09:36 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,17 @@
+//Q-Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+
+int main(){
+int n;
+scanf ("%d",&n);
+printf ("sum =%d", n*(n+1)/2); 
+    return 0;
+}
+/*PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q8.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe 
+5
+sum =15
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> gcc Q8.c
+PS C:\Users\Arya_Vardhan\OneDrive\Desktop\100 days coding> ./a.exe 
+10
+sum =55*/
